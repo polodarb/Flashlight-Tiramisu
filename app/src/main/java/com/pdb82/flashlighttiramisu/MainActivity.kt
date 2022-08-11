b091797d7620943aa2a088cb089e2b6b0898355b
Daniil <65498838+polodarb@users.noreply.github.com>
2022-08-11 21:33:28 +0300

    Purification of imports

--- app/src/main/java/com/pdb82/flashlighttiramisu/MainActivity.kt
+++ app/src/main/java/com/pdb82/flashlighttiramisu/MainActivity.kt
@@ -2,22 +2,16 @@ package com.pdb82.flashlighttiramisu
 
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Color
-import android.hardware.camera2.CameraAccessException
 import android.hardware.camera2.CameraCharacteristics
 import android.hardware.camera2.CameraManager
-import android.hardware.camera2.CameraMetadata
 import android.net.Uri
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.SeekBar
 import android.widget.TextView
-import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.core.view.WindowCompat
 
